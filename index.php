6a0fdc698198e8d607c7ffcdfcc13f30f537bcd6
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-09-21 19:37:34 +0300

    Show function gettype

--- index.php
+++ index.php
@@ -5,3 +5,14 @@
  * Date: 21.09.2017
  * Time: 19:21
  */
+$a ='test';
+$b = 1;
+$c = 1.1;
+$d = true;
+echo gettype($a);
+echo "<p>";
+echo gettype($b);
+echo "<p>";
+echo gettype($c);
+echo "<p>";
+echo gettype($d);
