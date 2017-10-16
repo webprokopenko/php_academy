00f0adc602fe1fe75e8317efb4944a61b97c7773
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-16 20:05:24 +0300

    build query

--- headers/3.php
+++ headers/3.php
@@ -4,4 +4,6 @@
  * User: SPARK
  * Date: 16.10.2017
  * Time: 19:59
+ * Удаление cookie
  */
+setcookie('flavor','',1);
---
+++ headers/4.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SPARK
+ * Date: 16.10.2017
+ * Time: 20:00
+ * Построение строки запроса
+ * Требуется сгенерировать строку запроса с заданными парами имя\значение
+ */
+$vars = ['name'=>'Oscar','color'=>'green','favorite'=>1];
+
+$quryString = http_build_query($vars);
+$url = '/headers/select.php?'.$quryString;
+$cookieString = http_build_query($vars);
+echo $url;
+echo "<br>";
+echo $cookieString;
