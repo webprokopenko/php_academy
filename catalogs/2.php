ef63e8fbddaf102334b0ccde5c3f514d3c49246a
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-19 19:46:18 +0300

    Function files

---
+++ catalogs/2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SPARK
+ * Date: 19.10.2017
+ * Time: 19:39
+ */
+file_exists(''); //Существует ли файл
+fileatime(''); //Время последнего обращения
+fileowner(''); //Владелец
+filesize(''); //Размер файла
+filetype(''); //Тип( dir,link,file,unknown
+is_dir('');// Директория или нет
+is_executable(''); // Является ли исполняемым фалом
+is_file(''); //Является ли файлом
+is_link(''); //Является ли ссылкой
+is_readable(''); //ДОступ для чтения
+is_writable(''); //Доступ для записи
