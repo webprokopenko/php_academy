5e0b4a58475731bbe1bc02391034af9614074ed4
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-11-13 19:32:01 +0200

    Final class method

---
+++ oop/simplesClass/FinalClass.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SPARK
+ * Date: 13.11.2017
+ * Time: 19:25
+ */
+final class FinalClass{
+
+}
+
+class simpleClass extends FinalClass{
+
+}
+
+$finalObject = new FinalClass();
+
+class simpleClass1{
+	final public function finalMethod() : bool
+	{
+		return true;
+	}
+	private function notFinalMethod() : bool{
+		return false;
+	}
+
+}
+
+class simpleClass2 extends simpleClass1{
+	public function finalMethod(){
+
+	}
+	public function notFinalMethod(): bool {
+		return true;
+	}
+}
+$simpleObject2 = new simpleClass2();
+$simpleObject2->notFinalMethod();
