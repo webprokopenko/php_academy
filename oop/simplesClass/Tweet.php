15d4772d68b199dcf6702b83e71ec3acf73c11df
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-11-13 20:49:21 +0200

    tweet

---
+++ oop/simplesClass/Tweet.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SPARK
+ * Date: 13.11.2017
+ * Time: 20:42
+ */
+class Tweet{
+	protected $data= [];
+
+	public function from(string $from){
+		$data['from'] = $from;
+		return $this;
+	}
+	public function withStatus(string $status){
+		$data['status'] = $status;
+		return $this;
+	}
+	public function send(){
+		return true;
+	}
+}
+
+$tweet = new Tweet();
+
+$id = $tweet->from("test@gmail.com")->withStatus('PHP 7')->send();
+
+var_dump($tweet->from('test@gmail.com'));
